3dfe8bdb10ccf1aa5599034186b053a6c85a7b1d
Ezekiel Mhando <mhandoezekiel@gmail.com>
2025-01-23 23:05:26 +0300

    rock paper scissors playRound $ output result

--- logic/jscript.js
+++ logic/jscript.js
@@ -1,14 +1,15 @@
 let humanScore = 0;
 let computerScore = 0;
+
 function getComputerChoice(){
     let computerChoice = "";
     let choice = Math.random();
     if (choice > 0 && choice <=0.3){
-        computerChoice = "rock";
+        computerChoice = "Rock";
     }else if (choice > 0.3 && choice <= 0.6) {
-        computerChoice = "paper";
+        computerChoice = "Paper";
     }else if (choice > 0.6 && choice < 1) {
-        computerChoice = "scissors";
+        computerChoice = "Scissors";
     }
     return computerChoice;
 }
@@ -25,5 +26,45 @@ function getHumanChoice () {
     }
     return humanChoice;
 }
-console.log(`Computer Chose: ${getComputerChoice()}`);
-console.log(`Human Chose: ${getHumanChoice()}`);
+function playRound ( computerChoic, humanChoic) {
+    const computerChoice = getComputerChoice();
+    const humanChoice = getHumanChoice();
+    let result = "";
+
+    if (humanChoice === "Rock"){
+        if (computerChoice === "Rock"){
+            result = "Its a tie";
+        }else if (computerChoice === "Paper"){
+            computerScore += 1;
+            result = "Computer won this round";
+        }else if (computerChoice === "Scissors"){
+            humanScore += 1;
+            result = "Human won this round";
+        }
+    }else if (humanChoice === "Paper"){
+        if (computerChoice === "Rock") {
+            humanScore += 1;
+            result = "Human won this round";
+        }else if (computerChoice === "Paper"){
+            result = "Its a tie";
+        }else if (computerChoice === "Scissors"){
+            computerScore += 1;
+            result = "Computer won this round";
+        }
+    }else if (humanChoice === "Scissors"){
+        if (computerChoice === "Rock"){
+            computerScore += 1;
+            result = "Computer won this round";
+        }else if (computerChoice === "Paper"){
+            humanScore += 1;
+            result = "Human won this round";
+        }else if (computerChoice === "Scissors"){
+            result = "This is a tie";
+        }
+    }
+
+    return result;
+}
+
+console.log(`${playRound()}\n ${getComputerChoice()}\n ${getHumanChoice()}`);
+
